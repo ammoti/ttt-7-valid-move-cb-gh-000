09e9d366c5b8498b69ef440a738e6b58c01a60ab
Vahap Yigit <vahap.yigit@gmail.com>
2017-11-09 15:32:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
 # code your #valid_move? method here
-
+def valid_move?(array,index)
+if(array[index]==" ")
+  return true
+elsif (array[index]!="X" || array[index]!="O")
+  return true
+elsif(array[index]=="X" || array[index]=="O")
+  return nil
+end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
